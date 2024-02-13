2ec8b4a882534d885f00ddc9244c7d32370600bf
Sarthak <srnegi141@gmail.com>
2024-02-13 12:57:32 -0600

    getting the command line arguments, tokenizing and printing them

--- shell.c
+++ shell.c
@@ -1,10 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 char *tokenArray;
 char *argumentList[10];
 
 int main()
 {
-    for (int i = 0; i < 10; i++)
+    printf("shell$ ");
+    int i = 0;
+    for (i = 0; i < 10; i++)
     {
-        argumentList[i] = (char *)malloc(100 * sizeof(char));
+        argumentList[i] = (char *)malloc(sizeof(char) * 81);
+        argumentList[i] = NULL;
+    }
+    char str[1000];
+    fgets(str, 1000, stdin);
+
+    tokenArray = strtok(str, " \n");
+
+    i = 0;
+
+    while (tokenArray != NULL)
+    {
+        if (i == 0)
+        {
+            char *command = strdup(tokenArray);
+        }
+
+        argumentList[i] = strdup(tokenArray);
+        tokenArray = strtok(NULL, " \n");
+        i++;
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        if (argumentList[i] != NULL)
+        {
+            printf("%s\n", argumentList[i]);
+        }
     }
 }
